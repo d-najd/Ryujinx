7e87205905c69140da2dc07d0bd96d50cbc2cf31
Dimitar <dimitar.najdovski@cythero.com>
2023-09-24 23:11:28 +0200

    Testing

---
+++ src/Ryujinx.Tests/Texture/BC7DecoderTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System.IO;
+using Ryujinx.Graphics.Texture;
+using Ryujinx.Graphics.Texture.Encoders;
+
+namespace Ryujinx.Tests.Texture
+{
+    public class BC7DecoderTests
+    {
+        private static string _rootDir;
+        private static string _imageSrc;
+        private static string _imageDest;
+
+        [SetUp]
+        public void Setup()
+        {
+            _rootDir = TestContext.CurrentContext.TestDirectory;
+            _imageSrc = $@"{_rootDir}\TestImg.png";
+            _imageDest = $@"{_rootDir}\TestImgD.png";
+        }
+
+        /// <summary>
+        /// Just testing if the encoder/decoder is properly set up, compressing and decompressing the image
+        /// </summary>
+        [Test]
+        public void Test()
+        {
+            TestContext.Out.WriteLine($"Test dir {_imageSrc}");
+            var fileData = File.ReadAllBytes(_imageSrc);
+
+            var encoded = BCnEncoder.EncodeBC7(fileData, 16, 16, 1, 1, 1);
+            var decoded = BCnDecoder.DecodeBC7(encoded, 16, 16, 1, 1, 1);
+            
+            File.WriteAllBytes(_imageDest, decoded);
+        }
+    }
+}
